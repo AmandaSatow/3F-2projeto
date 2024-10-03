82ca88941e6a6dfb53086106415f934faccc14aa
AmandaSatow <108934778+AmandaSatow@users.noreply.github.com>
2024-10-03 09:43:20 -0300

    3-10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,13 +2,19 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function visualizaDadosGlobais(){
     const res = await fetch(url)
-    const
-     dados = await res.json()
+    const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
     console.log(dados)
 
-    const paragrafo =  document.createElement('p')
-    paragrafo.classList.add('graficos-texto')
-    const  container =  document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-texto');
+    const  container =  document.getElementById('graficos-container');
+    paragrafo.innerHTML = `O mundo tem ${totalPessoasMundo} bilhoẽs de habitantes, 
+    dos quais ${totalPessoasConectadas} bilhoẽs estão conectados em alguma rede social 
+    por um tempo médio de ${tempoMedio}.`;
+    container.appendChild(paragrafo);
 }
 
 visualizaDadosGlobais();
