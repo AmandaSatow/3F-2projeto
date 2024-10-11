104376601b43c93f5a7048ef2c729a7d6bfb714f
AmandaSatow <108934778+AmandaSatow@users.noreply.github.com>
2024-10-11 08:56:00 -0300

    1110

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -3,8 +3,8 @@ async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json();
-    const nomeDasRedes = object.keys(dados)
-    const quantidadeDeUsuarios = obejct.values(dados)
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
 
     const infos = [
         {
