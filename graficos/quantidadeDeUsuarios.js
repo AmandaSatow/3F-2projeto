65f8193df26b52131706c885729f51cc837302bb
AmandaSatow <108934778+AmandaSatow@users.noreply.github.com>
2024-10-24 09:23:57 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -13,15 +13,29 @@ async function quantidadeDeUsuarios(){
             y:quantidadeDeUsuarios,
             type: 'bar', 
             marker: {
-                color: getCSS('--cor-secundaria'),
+                color: getCSS('--cor-secundaria')
             }
         }
     ]
 
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS("--cor-de-fundo"),
+        title:{
+            text:'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font:{
+                color: getCSS('--cor-primaria'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        }
+    }
+
     const grafico = document.createElement('div');
     grafico.className = 'grafico';
     document.getElementById('graficos-container').appendChild(grafico);
-    Plotly.newPlot(grafico, infos);
+    Plotly.newPlot(grafico, infos, layout);
 
 }
 
